0ff9bb21978a5570ecd651c37235513c99467537
bediaerdoan <92927291+bediaerdoan@users.noreply.github.com>
2024-08-01 13:39:05 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -5,13 +5,13 @@ import 'firebase/compat/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD32N-GopeDMLrXv5rMJNe1bIqJx4PqDI0",
-  authDomain: "tez-uygulama-auth.firebaseapp.com",
-  projectId: "tez-uygulama-auth",
-  storageBucket: "tez-uygulama-auth.appspot.com",
-  messagingSenderId: "427385257736",
-  appId: "1:427385257736:web:abbed5d4f1a5db40b40af8",
-  measurementId: "G-6MKR3QEQ0Q",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
 
 // Initialize Firebase
